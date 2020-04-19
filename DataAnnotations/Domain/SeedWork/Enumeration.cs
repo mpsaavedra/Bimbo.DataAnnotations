2e097faa54b7d9ee3da69c3c74b075509e5352fb
Michel Perez Saavedra <michel.perez.saavedra@gmail.com>
2020-04-18 23:46:35 -0400

    Adding and enumeration class to make enums adjustable

---
+++ DataAnnotations/Domain/SeedWork/Enumeration.cs
@@ -0,0 +1,101 @@
+﻿//
+// Enumeration.cs
+//
+// Author:
+//       Michel Perez Saavedra <michel.perez.saavedra@gmail.com>
+//
+// Copyright (c) 2020 Bimbo Team Software Technologies
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+//
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Bimbo.Domain.SeedWork
+{
+    public abstract class Enumeration : IComparable
+    {
+        public string Name { get; private set; }
+
+        public int Id { get; private set; }
+
+        protected Enumeration(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public override string ToString() => Name;
+
+        public static IEnumerable<T> GetAll<T>() where T : Enumeration
+        {
+            var fields = typeof(T).GetFields(BindingFlags.Public | BindingFlags.Static | BindingFlags.DeclaredOnly);
+
+            return fields.Select(f => f.GetValue(null)).Cast<T>();
+        }
+
+        public override bool Equals(object obj)
+        {
+            var otherValue = obj as Enumeration;
+
+            if (otherValue == null)
+                return false;
+
+            var typeMatches = GetType().Equals(obj.GetType());
+            var valueMatches = Id.Equals(otherValue.Id);
+
+            return typeMatches && valueMatches;
+        }
+
+        public override int GetHashCode() => Id.GetHashCode();
+
+        public static int AbsoluteDifference(Enumeration firstValue, Enumeration secondValue)
+        {
+            var absoluteDifference = Math.Abs(firstValue.Id - secondValue.Id);
+            return absoluteDifference;
+        }
+
+        public static T FromValue<T>(int value) where T : Enumeration
+        {
+            var matchingItem = Parse<T, int>(value, "value", item => item.Id == value);
+            return matchingItem;
+        }
+
+        public static T FromDisplayName<T>(string displayName) where T : Enumeration
+        {
+            var matchingItem = Parse<T, string>(displayName, "display name", item => item.Name == displayName);
+            return matchingItem;
+        }
+
+        private static T Parse<T, K>(K value, string description, Func<T, bool> predicate) where T : Enumeration
+        {
+            var matchingItem = GetAll<T>().FirstOrDefault(predicate);
+
+            if (matchingItem == null)
+                throw new InvalidOperationException($"'{value}' is not a valid {description} in {typeof(T)}");
+
+            return matchingItem;
+        }
+
+        public int CompareTo(object other) => Id.CompareTo(((Enumeration)other).Id);
+    }
+}
